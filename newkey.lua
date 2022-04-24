8f97e02ee026748b45b3da2ec4068e72fe3afd35
Nx <101421554+HazeNx@users.noreply.github.com>
2022-04-24 18:16:57 +0800

    Update newkey.lua

--- newkey.lua
+++ newkey.lua
@@ -240,7 +240,7 @@ function lib:Window(text, preset, closebind)
    MonkIcon.ImageColor3 = Color3.fromRGB(133, 115, 255)
    
 MinimizeBtn.MouseButton1Click:Connect(function()
-   Main:TweenSize(UDim2.new(0, 0, 0, 0),Enum.EasingDirection.Out,Enum.EasingStyle.Quart,
+   Main:TweenSize(UDim2.new(0, 0, 0, 0),Enum.EasingDirection.In,Enum.EasingStyle.Quart,
       .3,
       true
       )
