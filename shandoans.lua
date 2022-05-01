22e00baade72fd89a73345902138a2761459a03e
Nx <101421554+HazeNx@users.noreply.github.com>
2022-05-01 14:18:57 +0800

    Update shandoans.lua

--- shandoans.lua
+++ shandoans.lua
@@ -293,6 +293,23 @@ function lib:Window(text, preset, closebind)
    MonkIcon.Image = "rbxassetid://8825654232"
    MonkIcon.ImageColor3 = Color3.fromRGB(220, 221, 222)
    
+   function osfunc:Refresh(textadd)
+           Whitelist.Text = textadd
+      end
+    local function UpdateOS()
+            local date = os.date("*t")
+            local hour = (date.hour) % 24
+            local ampm = hour < 12 and "AM" or "PM"
+            local timezone = string.format("%02i:%02i:%02i %s", ((hour -1) % 12) + 1, date.min, date.sec, ampm)
+            osfunc:Refresh(timezone)
+        end
+        spawn(function()
+            while true do
+                UpdateOS()
+                game:GetService("RunService").RenderStepped:Wait()
+            end
+        end)
+   
    MinimizeBtn.MouseButton1Click:Connect(function()
        if minimized == false then
           Main:TweenSize(
@@ -321,24 +338,6 @@ function lib:Window(text, preset, closebind)
           HideGUI.Visible = false
         end
    end)
-   
-   
-   function osfunc:Refresh(textadd)
-        Whitelist.Text = textadd
-   end
- local function UpdateOS()
-         local date = os.date("*t")
-         local hour = (date.hour) % 24
-         local ampm = hour < 12 and "AM" or "PM"
-         local timezone = string.format("%02i:%02i:%02i %s", ((hour -1) % 12) + 1, date.min, date.sec, ampm)
-         osfunc:Refresh(timezone)
-     end
-     spawn(function()
-         while true do
-             UpdateOS()
-             game:GetService("RunService").RenderStepped:Wait()
-         end
-     end)
      
     local uitoggled = false
     UserInputService.InputBegan:Connect(
